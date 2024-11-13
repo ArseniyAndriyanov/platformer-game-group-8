c74f174b3659a51e9aac465662a2b5a3d90ba803
ArseniyAndriyanov <145904548+ArseniyAndriyanov@users.noreply.github.com>
2024-11-13 22:28:14 +0300

    Add files via upload

---
+++ Scripts/pauselevel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pauselevel : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+}
